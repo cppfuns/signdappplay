08d3a78a9c2be85fd14c31f3aa19c0b6e8e0b021
suweigang <suweigang@kingsoft.com>
2018-07-26 21:58:19 +0800

    first add

---
+++ signdappplay.cpp
@@ -0,0 +1,114 @@
+#include "signdappplay.hpp"
+
+void signdappplay::transfer(account_name from, account_name to, asset quantity, string memo)
+{
+    if (from == _self || to != _self) {
+        return;
+    }
+
+    // don't do anything on transfers from our reference account
+    if (from == N(signdappplay)) {
+      return;
+    }
+    
+    eosio_assert(quantity.symbol == CORE_SYMBOL, "only core token allowed"); //string_to_symbol(4, "EOS")
+    eosio_assert(quantity.is_valid(), "Invalid token transfer");
+    eosio_assert(quantity.amount > 0, "Quantity must be positive");
+
+    //memo "your_account_name-your_owner_public_key-your_active_public_key" 分隔符支持“-” “:” 空格
+    //去掉memo前面的空格
+    memo.erase(memo.begin(), find_if(memo.begin(), memo.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+    //去掉memo后面的空格
+    memo.erase(find_if(memo.rbegin(), memo.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), memo.end());
+
+    eosio_assert(transfer.memo.length() == 120 || transfer.memo.length() == 66, "Malformed Memo (not right length)");
+    const string account_string = transfer.memo.substr(0, 12);
+    const account_name account_to_create = string_to_name(account_string.c_str());
+    eosio_assert(transfer.memo[12] == ':' || transfer.memo[12] == '-' || transfer.memo[12] == ' ', "Malformed Memo [12] == : or - or space");
+
+    const string owner_key_str = transfer.memo.substr(13, 53);
+    string active_key_str;
+
+    if(transfer.memo[66] == ':' || transfer.memo[66] == '-' || transfer.memo[66] == ' ') {
+      // active key provided
+      active_key_str = transfer.memo.substr(67, 53);
+    } else {
+      // active key is the same as owner
+      active_key_str = owner_key_str;
+    }
+    
+    const abieos::public_key owner_pubkey =
+        abieos::string_to_public_key(owner_key_str);
+    const abieos::public_key active_pubkey =
+        abieos::string_to_public_key(active_key_str);
+
+    array<char, 33> owner_pubkey_char;
+    copy(owner_pubkey.data.begin(), owner_pubkey.data.end(),
+         owner_pubkey_char.begin());
+
+    array<char, 33> active_pubkey_char;
+    copy(active_pubkey.data.begin(), active_pubkey.data.end(),
+         active_pubkey_char.begin());
+
+    key_weight owner_pubkey_weight = {
+        .key = owner_pubkey_char,
+        .weight = (uint8_t)abieos::key_type::k1;
+    };
+
+    key_weight active_pubkey_weight = {
+        .key = active_pubkey_char,
+        .weight = (uint8_t)abieos::key_type::k1;
+    };
+
+    authority owner = authority{
+        .threshold = 1,
+        .keys = {owner_pubkey_weight},
+        .accounts = {},
+        .waits = {}
+    };
+
+    authority active = authority{
+        .threshold = 1,
+        .keys = {active_pubkey_weight},
+        .accounts = {},
+        .waits = {}
+    };
+
+    newaccount new_account = newaccount{
+        .creator = _self,
+        .name = new_account_name,
+        .owner = owner,
+        .active = active
+    };
+
+    asset stake_net(1000, CORE_SYMBOL);
+    asset stake_cpu(1000, CORE_SYMBOL);
+    asset buy_ram = quantity - stake_net - stake_cpu;
+    eosio_assert(buy_ram.amount > 0, "Not enough balance to buy ram");
+
+    // create account
+    action(
+            permission_level{ _self, N(active) },
+            N(eosio),
+            N(newaccount),
+            new_account
+    ).send();
+    // buy ram
+    action(
+            permission_level{ _self, N(active)},
+            N(eosio),
+            N(buyram),
+            make_tuple(_self, new_account_name, buy_ram)
+    ).send();
+    // delegate and transfer cpu and net
+    action(
+            permission_level{ _self, N(active)},
+            N(eosio),
+            N(delegatebw),
+            make_tuple(_self, new_account_name, stake_net, stake_cpu, true)
+    ).send();
+}
---
+++ signdappplay.hpp
@@ -0,0 +1,73 @@
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/asset.hpp>
+#include <eosiolib/types.hpp>
+#include <eosiolib/action.hpp>
+#include <eosiolib/symbol.hpp>
+#include <eosiolib/public_key.hpp>
+
+using namespace eosio;
+class signdappplay: public contract {
+public:
+    signdappplay(account_name self): contract(self){};
+    void transfer(account_name from, account_name to, asset quantity, string memo);
+
+private:
+    struct permission_level_weight {
+        permission_level permission;
+        weight_type weight;
+        EOSLIB_SERIALIZE(permission_level_weight, (permission)(weight))
+    };
+
+    struct key_weight {
+        eosio::public_key key;
+        weight_type weight;
+
+        EOSLIB_SERIALIZE(key_weight, (key)(weight))
+    };
+
+    struct wait_weight {
+        uint32_t wait_sec;
+        weight_type weight;
+
+        EOSLIB_SERIALIZE(wait_weight, (wait_sec)(weight))
+    };
+
+    struct authority {
+        uint32_t threshold;
+        vector<key_weight> keys;
+        vector<permission_level_weight> accounts;
+        vector<wait_weight> waits;
+
+        EOSLIB_SERIALIZE(authority, (threshold)(keys)(accounts)(waits))
+    };
+
+    struct newaccount {
+        account_name creator;
+        account_name name;
+        authority owner;
+        authority active;
+
+        EOSLIB_SERIALIZE(newaccount, (creator)(name)(owner)(active))
+    };
+};
+
+
+#define EOSIO_ABI_EX( TYPE, MEMBERS ) \
+extern "C" { \
+    void apply( uint64_t receiver, uint64_t code, uint64_t action ) { \
+        auto self = receiver; \
+        if( action == N(onerror)) { \
+            /* onerror is only valid if it is for the "eosio" code account and authorized by "eosio"'s "active permission */ \
+            eosio_assert(code == N(eosio), "onerror action's are only valid from the \"eosio\" system account"); \
+        } \
+        if((code == N(eosio.token) && action == N(transfer)) ) { \
+            TYPE thiscontract( self ); \
+            switch( action ) { \
+                EOSIO_API( TYPE, MEMBERS ) \
+            } \
+         /* does not allow destructor of thiscontract to run: eosio_exit(0); */ \
+        } \
+    } \
+} \
+
+EOSIO_ABI_EX(signdappplay, (transfer))
